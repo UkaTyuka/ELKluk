0afdcaad6acaab622008a5c6740816ff6d301e67
UkaTyuka <114570223+UkaTyuka@users.noreply.github.com>
2026-01-22 21:36:03 +0300

    Update outputs.tf

--- openstack/outputs.tf
+++ openstack/outputs.tf
@@ -1,4 +1,4 @@
-output "UkaTyuka_vm_ip" {
+output "UkaTyuka-terraform_ip" {
   description = "IP-адрес ВМ в сети sutdents-net"
-  value       = openstack_compute_instance_v2.elk_vm.network[0].fixed_ip_v4
+  value       = openstack_compute_instance_v2.UkaTyuka-terraform.network[0].fixed_ip_v4
 }
